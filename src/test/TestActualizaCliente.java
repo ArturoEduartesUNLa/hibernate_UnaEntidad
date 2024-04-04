545e41d3cf582d1bb7324a984357f5ec9d3f3eb5
ArturoEduartes <ArturoEduartes@users.noreply.github.com>
2024-04-04 17:33:17 -0300

    Test modificar cliente - completed

---
+++ src/test/TestActualizaCliente.java
@@ -0,0 +1,28 @@
+package test;
+
+import datos.Cliente;
+import negocio.ClienteABM;
+
+public class TestActualizaCliente {
+
+	public static void main(String[] args) {
+		
+		ClienteABM abm = new ClienteABM();
+		
+		Cliente cliente = abm.traer(1L);
+		System.out.println("cliente a modificar: \n" + cliente);
+		
+		cliente.setDni(35000004);
+		try {
+			abm.modificar(cliente);
+			System.out.println("Cliente Modif.:" + abm.traer(35000004));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
